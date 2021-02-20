58bce08e73be0f4f7a43ffc3df764026983e7b3e
Manuel Rodriguez <mr.1311223@gmail.com>
2021-02-19 20:15:44 -0400

    Utilizar debug en consola de vscode

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-const { port, dev }  = require('./config');
-
+const { port, dev } = require('./config');
 
 const app = require('./server');
+var debug = require('debug')('my-express-app:server');
+debug.log = console.debug.bind(console);
 
-app.listen(port, function() {
+app.listen(port, function () {
   dev === 'development'
     ? console.log(`Listening in DEVELOPMENT http://localhost:${port}`)
     : console.log(`Listening http://localhost:${port}`);
